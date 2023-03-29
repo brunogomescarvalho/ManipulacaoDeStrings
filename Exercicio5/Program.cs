32fc612cae617cfbb2779606381666cb7af772c4
brunogomescarvalho <7bruno8@gmail.com>
2023-03-29 14:37:50 -0300

    first commit

--- Exercicio5/Program.cs
+++ Exercicio5/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-          
+
+            Console.Clear();
+
+            Console.WriteLine("Informe a frase desejada:");
+            string frase = Console.ReadLine()!;
+
+            Console.WriteLine("\nFrase em Letras Maiúsculas:");
+            string fraseEmMaiusculas = frase.ToUpper();
+            Console.WriteLine(fraseEmMaiusculas);
+
+            Console.WriteLine("\nFrase em Letras Minúsculas:");
+            string fraseEmMinusculas = frase.ToLower();
+            Console.WriteLine(fraseEmMinusculas);
+
+            Console.WriteLine("\nQuantidade de Caracteres:");
+            Console.WriteLine(frase.Length);
+
+            string[] arrayPalavras = frase.Split(" ");
+
+            Console.WriteLine("\nPrimeira Palavra:");
+            Console.WriteLine(arrayPalavras[0]);
+
+            Console.WriteLine("\nÚltima Palavra:");
+            Console.WriteLine(arrayPalavras[arrayPalavras.Length - 1]);
 
         }
     }
